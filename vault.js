6358470c866b5b8613043a998f0e6b3621830e78
Sravan Taraknath <staraknath@yahoo.co.in>
2024-08-01 12:42:31 -0500

    Completed adding code to the files

---
+++ vault.js
@@ -0,0 +1,14 @@
+// first declare a variable with the required verbiage
+// declare 3 more variables, each with a mathematical calculation that calculates the 3 numbers required for the combination
+// concat the variables to form the full sentence required for the alert pop-up
+// display the alert
+
+const commonSentence = "You have received this message because you have been chosen to open an important vault. Here is the secret combination:"
+let firstNum = 8 + 2
+let secondNum = 2 * 2 * 10
+let thirdNum = 20 * 2 - 1
+
+const fullSentence = `${commonSentence} ${firstNum} - ${secondNum} - ${thirdNum}`
+
+alert(fullSentence)
+
