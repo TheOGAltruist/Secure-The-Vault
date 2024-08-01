18bd103dfe04b717709c1016d0fc07f6482d73cc
Sravan Taraknath <staraknath@yahoo.co.in>
2024-08-01 12:53:49 -0500

    Adding comments in the code

--- vault.js
+++ vault.js
@@ -3,12 +3,21 @@
 // concat the variables to form the full sentence required for the alert pop-up
 // display the alert
 
+//creating first variable with the required verbiage
 const commonSentence = "You have received this message because you have been chosen to open an important vault. Here is the secret combination:"
+
+// second variable for computing and storing the first number in the combination
 let firstNum = 8 + 2
+
+// third variable for computing and storing the first number in the combination
 let secondNum = 2 * 2 * 10
+
+// fourth variable for computing and storing the first number in the combination
 let thirdNum = 20 * 2 - 1
 
+// Concatenating all the variables to form the complete alert string
 const fullSentence = `${commonSentence} ${firstNum} - ${secondNum} - ${thirdNum}`
 
+// displaying the alert pop-up
 alert(fullSentence)
 
